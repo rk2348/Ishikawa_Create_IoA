f0a091718b024619a7959f9f0aaea8bf7b79e1bd
ryota <ryouximury127@gmail.com>
2025-10-03 23:11:40 +0900

    perf: 皿アセットのローポリゴンモデルへの置き換えによる描画負荷軽減

---
+++ Assets/Scripts/SetDefaultRotation.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SetDefaultRotation : MonoBehaviour
+{
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(-90f, 0f, 0f); // ƒ[ƒ‹ƒhŠî€‚Å‰ñ“]‚ğw’è
+    }
+}
